4cc3be07a919acb35dcfcb07b89888b283ccb895
jamel3ond <31301098+jamel3ond@users.noreply.github.com>
2022-08-15 17:01:48 +0700

    Update list project under folders and organization

--- drill_org.sh
+++ drill_org.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-rm -f $PWD/list_folders.csv
+rm -f $PWD/list_resource_hierarchy.csv
 
-ORGANIZATION_ID="[FILL_IN_ORGANIZATION_ID]"
+ORGANIZATION_ID="[Fil_in_Organization_ID]"
+project_uf_arr=()
 
 # gcloud format
 FORMAT="csv[no-heading](name,displayName.encode(base64))"
 
-echo Folder_Name,Folder_ID > $PWD/list_folders.csv
+echo Name,ID,type > $PWD/list_resource_hierarchy.csv
 
 # Enumerates Folders recursively
 folders()
@@ -21,10 +22,21 @@ folders()
 
     # Decodes the encoded name
     NAME=$(echo ${VALUES[1]} | base64 --decode)
-    printf "Folder: ${NAME},${FOLDER}\n"
     folder_name=${NAME}
     folder_id=${FOLDER}
-    echo $folder_name,$folder_id >> $PWD/list_folders.csv
+    type2="Folder"
+    printf "Folder: ${NAME},${FOLDER},${type2}\n"
+    echo $folder_name,$folder_id,$type2 >> $PWD/list_resource_hierarchy.csv
+
+    for listproject in $(gcloud projects list --filter 'parent.id='${FOLDER}' AND parent.type=folder' --format='value(projectId)')
+	do
+        project_id=$(gcloud projects describe $listproject --format='value(projectId)')
+        project_number=$(gcloud projects describe $listproject --format='value(projectNumber)')
+        type3="Project"
+        printf "Project: ${project_id},${project_number},${type3}\n"
+        echo $project_id,$project_number,$type3 >> $PWD/list_resource_hierarchy.csv
+        project_uf_arr+=($listproject)
+    done
     
     folders $(gcloud resource-manager folders list \
       --folder=${FOLDER} \
@@ -35,10 +47,11 @@ folders()
 
 # Start at the Org
 ORGANIZATION_NAME=$(gcloud organizations describe ${ORGANIZATION_ID} --format='value(displayName)')
-echo "Organization: ${ORGANIZATION_NAME},${ORGANIZATION_ID}"
 folder_name=${ORGANIZATION_NAME}
 folder_id=${ORGANIZATION_ID}
-echo $folder_name,$folder_id >> $PWD/list_folders.csv
+type1="Organization"
+echo "Organization: ${ORGANIZATION_NAME},${ORGANIZATION_ID},${type1}"
+echo $folder_name,$folder_id,$type1 >> $PWD/list_resource_hierarchy.csv
 
 LINES=$(gcloud resource-manager folders list \
   --organization=${ORGANIZATION_ID} \
@@ -46,3 +59,17 @@ LINES=$(gcloud resource-manager folders list \
 
 # Descend
 folders ${LINES[0]}
+
+# Projects that are not under the folder.
+project_nuf_arr=($(gcloud projects list --filter 'parent.id='${ORGANIZATION_ID}' AND parent.type=organization' --format='value(projectId)'))
+
+for project_nuf in "${project_nuf_arr[@]}"
+do
+
+    project_id=$(gcloud projects describe $project_nuf --format='value(projectId)')
+    project_number=$(gcloud projects describe $project_nuf --format='value(projectNumber)')
+    type3="Project"
+    printf "Project: ${project_id},${project_number},${type3}\n"
+    echo $project_id,$project_number,$type3 >> $PWD/list_resource_hierarchy.csv
+
+done
